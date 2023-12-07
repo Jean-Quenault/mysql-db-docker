47c981a835b53588b5f5ab6d782a18f1c4f1328c
Jean-Quenault <jeanform@proton.me>
2023-12-07 20:29:33 +0100

    Update

--- main.tf
+++ main.tf
@@ -32,17 +32,15 @@ resource "aws_instance" "instance_docker" {
   security_groups = [aws_security_group.instance_docker.name]
   user_data = base64encode(<<EOF
 #!/bin/bash
-yum update
-yum install docker -y
-yum install -y git 
-service docker start
-chkconfig docker on
-curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo yum update
+sudo yum install docker -y
+sudo yum install -y git 
+sudo service docker start
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/Jean-Quenault/docker-init
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0c877eee92397ec69.efs.eu-west-3.amazonaws.com:/ docker-init
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0c877eee92397ec69.efs.eu-west-3.amazonaws.com:/ docker-init
 cd docker-init/
-docker-compose up -d
 EOF
                         )
   tags = {
