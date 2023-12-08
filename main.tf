fe2be96d6fe7a6e90964e96cc329b315c125de42
Jean-Quenault <jeanform@proton.me>
2023-12-08 15:17:37 +0100

    update

--- main.tf
+++ main.tf
@@ -39,9 +39,10 @@ sudo service docker start
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo mkdir -p /home/ec2-user/dbdata
-sudo git clone https://github.com/Jean-Quenault/docker-init
+sudo mkdir -p /home/ec2-user/docker-init
+sudo git clone https://github.com/Jean-Quenault/docker-init /home/ec2-user/docker-init 
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-0c877eee92397ec69.efs.eu-west-3.amazonaws.com:/ /home/ec2-user/dbdata
-sudo cd docker-init/
+sudo cd /home/ec2-user/docker-init
 sudo docker-compose -f docker-compose.yml up -d
 EOF
                         )
